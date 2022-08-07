dbe35c19e4c1eab0efcadcd959c95d3b7d46abec
boonken <waynebkken@gmail.com>
2022-08-07 23:08:26 +0800

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Routes, Route } from "react-router-dom";
-import { Container } from "react-bootstrap";
-import { Home } from "./pages/Home";
-import { Store } from "./pages/Store";
-import { About } from "./pages/About";
-import { Navbar } from "./components/Navbar";
-import { ShoppingCartProvider } from "./context/ShoppingCartContext";
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { Home } from "./pages/Home"
+import { Store } from "./pages/Store"
+import { About } from "./pages/About"
+import { Navbar } from "./components/Navbar"
+import { ShoppingCartProvider } from "./context/ShoppingCartContext"
+
 function App() {
   return (
     <ShoppingCartProvider>
@@ -17,7 +18,7 @@ function App() {
         </Routes>
       </Container>
     </ShoppingCartProvider>
-  );
+  )
 }
 
-export default App;
+export default App
